66caa303083a8083b54f4f3f203fb75ce0e4d9b6
Fran <francisco20990@gmail.com>
2022-12-02 10:58:34 -0400

    Modificar tabla Users | Creación de tabla ROLES | SoftDelete to Users & Rol | Instalación doctrine/dbal

---
+++ database/migrations/2022_12_02_143452_edit_data_users_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->unsignedBigInteger('rol_id')->after('password');
+            $table->foreign('rol_id')->references('id')->on('roles');
+            $table->string('last_name')->after('name');
+            $table->string('dni')->after('last_name');
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            //
+        });
+    }
+};
