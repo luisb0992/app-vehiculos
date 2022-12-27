eca1f68cb02364dc19b99e249e506bb5bab66b26
Fran <francisco20990@gmail.com>
2022-12-27 05:50:34 -0400

    Init Bitacora :art:

--- app/Models/Workshop.php
+++ app/Models/Workshop.php
@@ -4,10 +4,13 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Spatie\Activitylog\Traits\LogsActivity;
+use Spatie\Activitylog\LogOptions;
+use Spatie\Activitylog\Contracts\Activity;
 
 class Workshop extends Model
 {
-    use SoftDeletes;
+    use SoftDeletes,LogsActivity;
 
     /**
      * The table associated with the model.
@@ -24,6 +27,19 @@ class Workshop extends Model
     protected $fillable = [
         'name',
     ];
+
+    public function getActivitylogOptions(): LogOptions
+    {
+        return LogOptions::defaults()
+        ->logOnly(['name']);
+        // Chain fluent methods for configuration options
+    }
+
+    public function tapActivity(Activity $activity, string $eventName)
+    {
+        $activity->ip = request()->ip();
+        $activity->user_agent = request()->userAgent();
+    }
 
     public function user(){
         return $this->hasOne(User::class);
