9089914694b127d5c71c320ff2666c97a73b65c7
Fran <francisco20990@gmail.com>
2023-01-12 20:04:24 -0400

    Agregar Modal para ver en detalle los datos del vehiculos en reportes (Contrucción)

---
+++ resources/js/Pages/Reports/Vehicle/components/ModalVehicle.vue
@@ -0,0 +1,29 @@
+<template lang="">
+    <Dialog header="Header" v-model:visible="open" :breakpoints="{'960px': '75vw', '640px': '90vw'}" :style="{width: '50vw'}" :maximizable="true" :modal="true">
+            <p class="m-0">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+                Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            <template #footer>
+                <Button label="No" icon="pi pi-times" @click="closeMaximizable" class="p-button-text"/>
+                <Button label="Yes" icon="pi pi-check" @click="closeMaximizable" autofocus />
+            </template>
+        </Dialog>
+</template>
+<script setup>
+import Dialog from 'primevue/dialog';
+import Button from 'primevue/button';
+import {ref} from 'vue'
+
+const props = defineProps({
+    displayMaximizable : Boolean
+});
+
+//alert(props.displayMaximizable);
+
+const open = ref(props.displayMaximizable);
+
+const closeMaximizable = () => {
+            open.value = false;
+        };
+
+</script>
--- resources/js/Pages/Reports/Vehicle/components/Table.vue
+++ resources/js/Pages/Reports/Vehicle/components/Table.vue
@@ -87,9 +87,6 @@
                 <Badge v-if="data.status == 3" value="Mantenimiento" severity="info" class="mr-2"></Badge>
                 <Badge v-if="data.status == 4" value="Reparado" severity="warning" class="mr-2"></Badge>
                 <Badge v-if="data.status == 5" value="Eliminado" severity="danger"></Badge>
-                <!-- <Badge v-if="data.repair_orders.length == 0"  severity="warning" >
-                    {{$page.props.status.repair_order.not_order}}
-                </Badge> -->
             </template>
         </Column>
         <Column field="dock">
@@ -107,7 +104,29 @@
                {{formatCurrency(slotProps.data.total)}}
             </template>
         </Column>
+        <Column style="min-width:8rem">
+            <template #body="{data}">
+                <div class="flex justify-between">
+                    <button type="button" @click="openMaximizable" class="inline-block px-6 py-2 border-2 border-blue-600 text-blue-600 font-medium text-xs leading-tight uppercase rounded-full hover:bg-black hover:bg-opacity-5 focus:outline-none focus:ring-0 transition duration-150 ease-in-out">
+                        <i class="pi pi-eye" />
+                    </button>
+                    <a target="_blank" class="inline-block px-6 py-2 border-2 border-red-600 text-red-600 font-medium text-xs leading-tight uppercase rounded-full hover:bg-black hover:bg-opacity-5 focus:outline-none focus:ring-0 transition duration-150 ease-in-out">
+                        <i class="pi pi-file-pdf" />
+                    </a>
+                </div>
+            </template>
+        </Column>
     </DataTable>
+    <Dialog header="Header" v-model:visible="displayMaximizable" :breakpoints="{'960px': '75vw', '640px': '90vw'}" :style="{width: '50vw'}" :maximizable="true" :modal="true">
+            <p class="m-0">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+                Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            <template #footer>
+                <Button label="No" icon="pi pi-times" @click="closeMaximizable" class="p-button-text"/>
+                <Button label="Yes" icon="pi pi-check" @click="closeMaximizable" autofocus />
+            </template>
+        </Dialog>
+    <!-- <ModalVehicle :displayMaximizable="displayMaximizable" /> -->
 </template>
 
 <script setup>
@@ -119,6 +138,10 @@ import InputText from 'primevue/inputtext';
 import Badge from 'primevue/badge';
 import ColumnGroup from 'primevue/columngroup';
 import Row from 'primevue/row';
+import { Link } from '@inertiajs/inertia-vue3'
+import ModalVehicle from './ModalVehicle.vue';
+import Dialog from 'primevue/dialog';
+import Button from 'primevue/button';
 
 
 const props = defineProps({
@@ -127,6 +150,11 @@ const props = defineProps({
 });
 const showdropdownDowload = ref(true);
 const loading1 = ref(true);
+const displayMaximizable = ref(false);
+const openMaximizable = () => {
+
+            displayMaximizable.value = true;
+        };
 
 onMounted(() => {
     loading1.value = false;
