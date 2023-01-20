0203c63741a875558dec5e22fad2e3bf1aee8608
luis barrios <luisbarrios0992@gmail.com>
2023-01-19 21:08:20 -0400

    agregando permisos api para admin y registrador

--- routes/modules/api.php
+++ routes/modules/api.php
@@ -4,7 +4,7 @@ use App\Http\Controllers\ApiController;
 use Illuminate\Support\Facades\Route;
 
 // rutas con middleware auth y prefix brands
-Route::middleware(['auth', 'is_recorder'])->prefix('external_api')->group(function () {
+Route::middleware(['auth', 'is_superadmin_or_recorder'])->prefix('external_api')->group(function () {
 
   // guardar un marca
   Route::post('search', [ApiController::class, 'searchVehicle'])
