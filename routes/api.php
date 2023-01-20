caf58179f955d11cb74632409fb1fba1aae3ec03
Fran <francisco20990@gmail.com>
2023-01-19 21:02:11 -0400

    Delete API TEST :art:

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Http\Controllers\Test\Api\TestController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -18,5 +17,3 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
-
-Route::post('test/vehicle', TestController::class . '@testApi')->name('test.api.vehicle');
